b81b427ff9adda40b47e0d33e961cacbd78cd979
Daniel Wu <wuon@protonmail.com>
2019-02-09 19:39:46 -0500

    date picker

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const moment = require('moment');
 const meals = require('../services/meals');
 
 const router = express.Router();
+
+router.get('/', (req, res) => {
+  let start = moment().startOf('day').toDate();
+  let end = moment().endOf('day').toDate();
+  if (req.query.start && req.query.end) {
+    start = moment(req.query.start).startOf('day').toDate();
+    end = moment(req.query.end).endOf('day').toDate();
+  }
+  meals.find(start, end)
+    .then((response) => {
+      res.status(200).json({
+        isSuccess: true,
+        log: response,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        isSuccess: false,
+        error,
+      });
+    });
+});
 
 router.get('/', (req, res) => {
   meals.find()
@@ -30,12 +53,19 @@ router.post('/', async (req, res) => {
 
 router.put('/', async (req, res) => {
   if (req.body.id) {
-    meals.insertMeal(req).then((response) => {
-      res.status(200).json({
-        isSuccess: true,
-        response,
+    meals.insertMeal(req)
+      .then((log) => {
+        res.status(200).json({
+          isSuccess: true,
+          log,
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({
+          isSuccess: false,
+          error,
+        });
       });
-    });
   } else {
     res.status(402).json({
       isSuccess: false,
@@ -43,6 +73,5 @@ router.put('/', async (req, res) => {
     });
   }
 });
-
 
 module.exports = router;
--- services/meals.js
+++ services/meals.js
@@ -1,5 +1,4 @@
 const mongodb = require('mongodb');
-const moment = require('moment');
 
 const loadMealsCollection = () => mongodb.MongoClient.connect(
   process.env.MONGODB_URI,
@@ -8,46 +7,24 @@ const loadMealsCollection = () => mongodb.MongoClient.connect(
   },
 ).then(client => client.db(process.env.MONGODB_USER).collection('meals'));
 
-module.exports.find = () => loadMealsCollection()
-  .then(meals => meals.find({}).toArray().then(query => query));
+module.exports.find = (start, end) => loadMealsCollection()
+  .then(meals => meals.find({
+    createdAt: {
+      $gte: start,
+      $lt: end,
+    },
+  }).toArray().then(query => query));
 
 module.exports.insertOne = () => loadMealsCollection()
-  .then(meals => meals.insertOne({ meals: [] }).then(response => response.ops[0]));
-
-/*
-
-router.patch('/:id', async (req, res) => {
-  const channels = await loadChannelsCollection();
-  if(req.params.id && (req.body.track || req.body.listeners)){
-    req.body.updatedAt = new Date();
-    await channels.findOneAndUpdate({
-      _id: req.params.id,
-    }, {
-      $set: req.body,
-    }, {
-      returnOriginal: false
-    },(err, response) => {
-      if(err){
-        console.log(err);
-      }else{
-        ioClient.emit('propagate', response.value);
-      }
-    });
-    res.status(200).send();
-  }else{
-    res.status(400).send();
-  }
-});
- */
-
+  .then(meals => meals.insertOne({
+    meals: [],
+    createdAt: new Date(),
+  }).then(response => response.ops[0]));
 
 module.exports.insertMeal = req => loadMealsCollection()
   .then(meals => meals.findOneAndUpdate({
     _id: mongodb.ObjectId(req.body.id),
   }, {
-    $set: {
-      createdAt: moment().format(),
-    },
     $push: {
       meals: {
         calories: req.body.meal.calories || 0,
@@ -57,9 +34,7 @@ module.exports.insertMeal = req => loadMealsCollection()
     },
   }, {
     returnOriginal: false,
-  }).then((response) => {
-    console.log(response);
-  }));
+  }).then(response => response.value));
 /*
 .then(meals => meals.findOneAndUpdate({
   _id: req.body.id,
